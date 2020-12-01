7fce4220005b20354cce392b3e8f8ff7c9889331
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2020-12-01 09:48:01 +0100

    improved day01

--- src/day01.py
+++ src/day01.py
@@ -1,6 +1,7 @@
 f = open("../input/input01")
 data = f.read().split()
 data = [int(i) for i in data]
+data.sort()
 
 no_solution1 = True
 no_solution2 = True
@@ -10,7 +11,7 @@ for i in range(len(data)):
         for k in range(len(data)):
             if no_solution1 and data[i] + data[j] == 2020:
                 print(f"Solution 1: {data[i] * data[j]}")
-                solution1 = False
+                no_solution1 = False
             if no_solution2 and data[i] + data[j] + data[k] == 2020:
                 print(f"Solution 2: {data[i] * data[j] * data[k]}")
-                solution2 = False
+                no_solution2 = False

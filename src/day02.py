1d8dfe620d09af626257ccdbd37a329b47e88270
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2020-12-02 10:07:27 +0100

    Added Day02

--- src/day02.py
+++ src/day02.py
@@ -2,4 +2,21 @@ import sys
 
 f = open(sys.argv[1])
 data = f.read().split()
-data = [int(i) for i in data]
+
+count1 = 0
+count2 = 0
+for i in range(0, len(data), 3):
+    minmax = data[i].split('-')
+    minimum = int(minmax[0])
+    maximum = int(minmax[1])
+    search_char = data[i + 1][0]
+    search_string = data[i + 2]
+    if minimum <= search_string.count(
+            search_char) and maximum >= search_string.count(search_char):
+        count1 += 1
+    if (search_string[minimum - 1] == search_char) ^ (search_string[maximum - 1]
+                                                      == search_char):
+        count2 += 1
+
+print(f'Part 1: {count1}')
+print(f'Part 2: {count2}')
---
+++ src/day03.py
@@ -0,0 +1,4 @@
+import sys
+
+f = open(sys.argv[1])
+data = f.read().split()

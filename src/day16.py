f2865e81e965d70f4816199656e453897eff91d2
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2020-12-26 23:12:51 +0100

    Day16 Part2 (CSP by hand)

--- src/day16.py
+++ src/day16.py
@@ -1,13 +1,14 @@
 import sys
+from collections import defaultdict
 
 f = open(sys.argv[1])
 data = f.read().strip().splitlines()
 data = [d for d in data if d.strip() != '']
-
 rules = dict()
 rules_section = True
 my_ticket = None
 tickets = list()
+departure_rules = list()
 
 for i, d in enumerate(data):
     if d == 'your ticket:':
@@ -16,10 +17,12 @@ for i, d in enumerate(data):
     elif d == 'nearby tickets:':
         for k in range(i + 1, len(data)):
             tickets.append([int(x) for x in data[k].split(',')])
-        break
+        break 
     elif rules_section:
         tmp = d.split(': ')
         rule_name = tmp[0]
+        if 'departure' in rule_name:
+            departure_rules.append(rule_name)
         rule_range = [x.split('-') for x in tmp[1].split(' or ')]
         rules[rule_name] = [(int(x[0]), int(x[1])) for x in rule_range]
 
@@ -35,7 +38,54 @@ for i in range(len(tickets)):
                     valid = True
                     break
         if not valid:
-            invalid.append(i)
+            invalid.append(tickets[i])
             error_rate += j
 
 print(f'Part 1: {error_rate}')
+
+
+def check_rule_at_pos(ticket, rule, pos):
+    if (ticket[pos] >= rule[0][0] and
+            ticket[pos] <= rule[0][1]) or (ticket[pos] >= rule[1][0] and
+                                           ticket[pos] <= rule[1][1]):
+        return True
+    return False
+
+
+rule_index = defaultdict(list)
+
+for r in rules:
+    rule = rules[r]
+    for pos in range(len(my_ticket)):
+        good_pos = True
+        for ticket in tickets:
+            if ticket not in invalid:
+                if not check_rule_at_pos(ticket, rule, pos):
+                    good_pos = False
+                    break
+        if good_pos:
+            rule_index[r].append(pos)
+
+table = list()
+for i, k in enumerate(rule_index):
+    table.append([])
+    for j in range(len(my_ticket)):
+        if j in rule_index[k]:
+            if j < 10:
+                table[i].append('0' + str(j))
+            else:
+                table[i].append(str(j))
+        else:
+            table[i].append('xx')
+
+for x in table:
+    for k in x:
+        print(k, end=' ')
+    print()
+
+product = 1
+indice = [2, 14, 7, 19, 16, 13]
+for x in indice:
+    product *= my_ticket[x]
+
+print(f'Part 2: {product}')

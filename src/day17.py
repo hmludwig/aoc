3326792746198eddb8bc4375905241ac5526bbd3
Harald Ludwig <23039398+hmludwig@users.noreply.github.com>
2021-03-28 12:26:47 +0200

    Changes

--- src/day17.py
+++ src/day17.py
@@ -2,9 +2,6 @@ import sys
 
 f = open(sys.argv[1])
 data = f.read().strip().splitlines()
-
-sum1 = 0
-for d in data:
-    sum1 += eval(d)
-
-print(f'Part 1: {sum1}')
+print(data)
+print(data[0])
+print(data[0][0])
